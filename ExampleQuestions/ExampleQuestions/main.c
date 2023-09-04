db01e7cdb857401272d4e530c5885c0db8bb1902
GkhKaya <gkhkaya00@gmail.com>
2023-09-04 12:48:28 +0300

    Example Questions part 2

--- ExampleQuestions/ExampleQuestions/main.c
+++ ExampleQuestions/ExampleQuestions/main.c
@@ -36,11 +36,46 @@ int main(int argc, const char * argv[]) {
     }*/
     
     //MARK: - code a program that takes the cube of the number entered from the keyboard
-    int a,b;
+    /*int a,b;
     printf("Plese enter a number: ");
     scanf("%d", &a);
     b = a*a*a;
-    printf("%d of cube is = %d \n", a,b);
+    printf("%d of cube is = %d \n", a,b);*/
+    
+    //MARK: - One type of bacteria reproduces by dividing itself in half every hour. In this laboratory How many bacteria will there be after 24 hours?
+    
+    /*int i,a;
+    a = 1;
+    for (i=1; i<=24; i++) {
+        a = a*2;
+    }
+    printf("%d",a);*/
+    
+    //MARK: - Code a program that takes 4 whole numbers from the keyboard and finds their sum.
+   /* int a,b,c,d,e;
+    printf("Enter first number: ");
+    scanf("%d",&a);
+    printf("Enter second number: ");
+    scanf("%d",&b);
+    printf("Enter third number: ");
+    scanf("%d",&c);
+    printf("Enter fourth number: ");
+    scanf("%d",&d);
+    e = a+b+c+d;
+    printf("Their sum is = %d \n", e);*/
+    
+    //MARK: - Numbers entered from the keyboard until 0 is entered: program that adds
+    /*int a,b = 0;
+    
+    while (a != 0) {
+        printf("Please enter a number = ");
+        scanf("%d", &a);
+        b = b+a;
+        
+    }
+    printf("%d",b);*/
+    
+    
     
     return 0;
 }
